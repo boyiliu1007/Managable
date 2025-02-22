d9384837f0abe92b733a187ce15f41fbbbef7ac3
boyiliu1007 <119803346+boyiliu1007@users.noreply.github.com>
2025-02-22 16:06:28 +0800

    add: initialize HomePage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,5 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo w-[20%] h-[50%]" src="./assets/logo.svg" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <router-view/>
 </template>
 
 <style scoped>
@@ -22,20 +7,11 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<!-- src/pages/Home.vue -->
+<template>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the homepage of your Vue 3 application.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
