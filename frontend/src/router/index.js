9cf8d6c60eb4169194e7c04415ea07bc11df83df
112121py <patty0988594268@gmail.com>
2025-02-25 23:41:25 +0800

    add login and error signup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,30 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
+import Login from '@/pages/login.vue'
+import Signup from '@/pages/signup.vue'
+
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
   {
     path: '/home',
     name: 'Home',
     component: Home,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup,
   },
 ];
 
